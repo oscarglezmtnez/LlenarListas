c11414b4bed305832d02d691997884c7a4caab25
Óscar González Martínez <oscarglezmtnez@msn.com>
2021-01-25 12:47:16 +0100

    Inicial 250121 12:45

---
+++ FuncionesComboBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace BibliotecaDeFunciones
+{
+    public class FuncionesComboBox
+    {
+        public void RellenarComboBox(ComboBox cbb, string selectCmd, string cadenadeconexion)
+        {
+            cbb.Items.Clear();
+            OleDbConnection cnx = new OleDbConnection(cadenadeconexion);
+            try
+            {
+                cnx.Open();
+                OleDbCommand cmd = new OleDbCommand(selectCmd, cnx);
+                OleDbDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    cbb.Items.Add(dr[0].ToString().ToUpper());
+                }
+
+                cnx.Close();
+                cbb.Items.Insert(0, "---> Selecciona un item");
+                cbb.SelectedIndex = 0;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
